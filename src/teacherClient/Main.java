975cf7dc603bc5dc00919baa0ec75e79b6cbfe06
snydecor000 <snydecor000@students.centergrove.k12.in.us>
2018-01-18 22:45:55 -0500

    Wow finally sends an email

--- src/teacherClient/Main.java
+++ src/teacherClient/Main.java
@@ -1,5 +1,100 @@
 package teacherClient;
 
-public class Main {
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.Properties;
 
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class Main
+{
+	
+	private static String USER_NAME = "";
+	private static String PASSWORD = "";
+	private static String RECIPIENT = "";
+	
+	public static void saveImage(String imageUrl, String destinationFile) throws IOException 
+	{
+	    URL url = new URL(imageUrl);
+	    InputStream is = url.openStream();
+	    OutputStream os = new FileOutputStream(destinationFile);
+	
+	    byte[] b = new byte[2048];
+	    int length;
+	
+	    while ((length = is.read(b)) != -1) {
+	        os.write(b, 0, length);
+	    }
+	
+	    is.close();
+	    os.close();
+	}
+	
+	public static void main(String[] args) 
+	{
+	    String from = USER_NAME;
+	    String pass = PASSWORD;
+	    String[] to = { RECIPIENT };
+	    String subject = "MassPass";
+	    
+	    sendFromGMail(from, pass, to, subject);
+	}
+	
+	private static void sendFromGMail(String from, String pass, String[] to, String subject) 
+	{
+	    Properties props = System.getProperties();
+	    String host = "smtp.gmail.com";
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.smtp.host", host);
+	    props.put("mail.smtp.user", from);
+	    props.put("mail.smtp.password", pass);
+	    props.put("mail.smtp.port", "587");
+	    props.put("mail.smtp.auth", "true");
+	
+	    Session session = Session.getDefaultInstance(props);
+	    MimeMessage message = new MimeMessage(session);
+	    
+	    try 
+	    {
+	        message.setFrom(new InternetAddress(from));
+	        InternetAddress[] toAddress = new InternetAddress[to.length];
+	
+	        // To get the array of addresses
+	        for( int i = 0; i < to.length; i++ ) 
+	        {
+	            toAddress[i] = new InternetAddress(to[i]);
+	        }
+	
+	        for( int i = 0; i < toAddress.length; i++) 
+	        {
+	            message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+	        }
+	
+	        message.setSubject(subject);
+	        
+	        Transport transport = session.getTransport("smtp");
+	        transport.connect(host, from, pass);
+	        transport.sendMessage(message, message.getAllRecipients());
+	        transport.close();
+	    }
+	    catch (AddressException ae) 
+	    {
+	        ae.printStackTrace();
+	    }
+	    catch (MessagingException me) 
+	    {
+	        me.printStackTrace();
+	    }
+	}
 }
